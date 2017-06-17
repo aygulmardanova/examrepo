3945f437afacaf158d3373ca64a9fd64a48a7bd6
aygulmardanova <mardanovaaigul@mail.ru>
2017-06-17 12:19:19 +0300

    4: new feature implemented

--- src/main/java/ru/kpfu/itis/fujitsu/exam/Hello.java
+++ src/main/java/ru/kpfu/itis/fujitsu/exam/Hello.java
@@ -3,6 +3,8 @@ package ru.kpfu.itis.fujitsu.exam;
 public class Hello {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello1");
         System.out.println("Hello!");
     }
 
